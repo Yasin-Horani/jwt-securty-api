84d1e9bf3ab563fdb0f26ff19e32b718fe095bc9
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-23 21:07:43 +0200

    init project

---
+++ src/main/java/com/yasin/jwtsecutyapi/JwtSecutyApiApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.jwtsecutyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecutyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecutyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yasin/jwtsecutyapi/JwtSecutyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasin.jwtsecutyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecutyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
