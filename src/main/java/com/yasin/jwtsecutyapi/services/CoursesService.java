cfe05a5c22ccbee1bdd37281cd1408018fbff320
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-24 23:32:19 +0200

    init Course service

---
+++ src/main/java/com/yasin/jwtsecutyapi/services/CoursesService.java
@@ -0,0 +1,24 @@
+package com.yasin.jwtsecutyapi.services;
+
+import com.yasin.jwtsecutyapi.model.dto.CourseDTO;
+import com.yasin.jwtsecutyapi.repository.CourseRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CoursesService {
+    private final CourseRepo courseRepo;
+
+    public CoursesService(CourseRepo courseRepo) {
+        this.courseRepo = courseRepo;
+    }
+
+    public List<CourseDTO> courses() {
+        return this.courseRepo.findAll();
+    }
+
+    public List<CourseDTO> findAllByUserId(Integer userId){
+        return this.courseRepo.findAllByUserId(userId);
+    }
+}
