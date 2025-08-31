34b811a3ce8efd5af44775d8dbab7ed469daed1a
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-31 19:27:44 +0200

    UPDATE user controller

--- src/main/java/com/yasin/jwtsecutyapi/controller/CoureseController.java
+++ src/main/java/com/yasin/jwtsecutyapi/controller/CoureseController.java
@@ -2,6 +2,8 @@ package com.yasin.jwtsecutyapi.controller;
 
 import com.yasin.jwtsecutyapi.model.dto.CourseDTO;
 import com.yasin.jwtsecutyapi.services.CoursesService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,12 @@ public class CoureseController {
     }
 
     @GetMapping(path = "/my-courses") // user
-    public List<CourseDTO> myCourses() {
-        return coursesService.findAllByUsername("user");
+    public List<CourseDTO> myCourses(@AuthenticationPrincipal Jwt jwt) {
+        var username = jwt.getSubject();
+        return coursesService.findAllByUsername(username);
     }
 
-    @GetMapping(path = "/my-courses/{userId}") // admin
+    @GetMapping(path = "/my-courses/{username}") // admin
     public List<CourseDTO> anyUserCourses(@PathVariable String username) {
         return coursesService.findAllByUsername(username);
     }
